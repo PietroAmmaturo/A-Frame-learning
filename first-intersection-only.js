e8f2bcf88f76d114592c3323fff06b9229534c66
pietro <pammaturo02@gmailcom>
2021-10-07 13:56:14 +0200

    useful function saved

--- first-intersection-only.js
+++ first-intersection-only.js
@@ -33,3 +33,26 @@ AFRAME.registerComponent('first-intersection-only', {
       return 0;
     }
   }
+
+  AFRAME.registerComponent('raycaster-listen', {
+    init: function () {
+      // Use events to figure out what raycaster is listening so we don't have to
+      // hardcode the raycaster.
+      this.el.addEventListener('raycaster-intersected', evt => {
+        console.log("raycaster-intersected", this.el);
+        this.raycaster = evt.detail.el;
+
+      });
+      this.el.addEventListener('raycaster-intersected-cleared', evt => {
+        console.log("raycaster-intersected-cleared", this.el);
+        this.raycaster = null;
+      });
+    },
+
+    tick: function () {
+      if (!this.raycaster) { return; }  // Not intersecting.
+
+      let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+      if (!intersection) { return; }
+    }
+  });
